58c138b5666153c139426421f949a9e87eb69345
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-09-21 16:29:42 +0300

    Minor change

--- schemas/surveySchema.js
+++ schemas/surveySchema.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const surveySchema = new mongoose.Schema(
   {
-    "Giving a gift, would it put a strain on your finances for the month?": String,
+    "Giving a gift, would it put a strain on your finances for the month?":
+      String,
     "How old are you?": Number,
     "How many houses do you own?": Number,
     "How many children do you have?": Number,
